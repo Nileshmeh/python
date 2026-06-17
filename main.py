8a3dcbe569937c81e82cb7ae373362ea67065933
Nileshmeh <teengardener07@gmail.com>
2026-06-17 21:54:52 +0530

    initial python commit

---
+++ main.py
@@ -0,0 +1,399 @@
+# # # # # #print("I'm a dumbass")
+# # # # # #print("kyaa re chikne")
+# # # # # #print("hi bbg <3") 
+
+# # # # # # strings
+
+# # # # # #first_name = "Nilesh"
+# # # # # #first_love = "my bbg"
+# # # # # #friends = "Niggas"
+# # # # # #close_friend = "Wigga"
+
+
+# # # # # #print(f"my first name is {first_name}")
+# # # # # #print(f"I lub u {first_love}")
+# # # # # #print(f"I love my {friends}")
+# # # # # #print(f"I love my {close_friend} too much")
+
+# # # # # #integers
+
+# # # # # #school_grades = 81
+# # # # # #targeted_sgpa = 9
+# # # # # #pocket_money = 150
+# # # # # #my_age = 18
+
+# # # # # #print(f"I got decent grades in boards which is : {school_grades}")
+# # # # # #print(f"i'll try to get this sgpa in collage : {targeted_sgpa}")
+# # # # # #print(f"i have currently {pocket_money}$ in my pocket")
+# # # # # #print(f"my age is: {my_age}")
+
+
+# # # # #  #boolians
+
+# # # # # #friend_online = True
+
+# # # # # #if friend_online:
+# # # # #  #   print("my friend is gay")
+# # # # # #else:
+# # # # #  #   print("my friend is straight")
+
+# # # # # #friend_offline = False
+
+# # # # # #if friend_offline:
+# # # # #  #   print("my friend is not gay")
+# # # # # #else:
+# # # # #  #   print("wigga is gay")
+
+
+# # # # # # typecasting
+
+# # # # # #my_name = "Nilu"
+# # # # # #grades_got = 81
+# # # # # #cgpa_got = 8.7
+# # # # # #always_stay = True
+
+
+# # # # # #grades_got = str(grades_got)
+# # # # # #cgpa_got = int(cgpa_got)
+# # # # # #my_name = bool(my_name)
+
+
+
+# # # # # #grades_got += "1"
+
+
+
+
+# # # # # #print(grades_got)
+# # # # # #print(type(cgpa_got))
+# # # # # #print(my_name)
+
+
+# # # # # # inputs
+
+# # # # # #length = float(input("length of the rectangle:"))
+# # # # # #width = float(input("width of the rectangle:"))
+
+# # # # # #area = length * width
+
+# # # # # #print("congratulations you got the answer")
+# # # # # #print(f"your area of rectangle is: {area}")
+
+
+# # # # # # madlibs game
+
+
+# # # # # #noun1 = input("enter a noun word: ")
+# # # # # #adjective1 = input("enter a adjective word: ")
+# # # # # #noun2 = input("enter another noun word: ")
+
+
+# # # # # #print(f"I always love my bbg {noun1}")
+# # # # # #print(f"he's so so {adjective1}")
+# # # # # #print(f"I saw him yesterday at {noun2}")
+
+
+# # # # # # arithmatic and mathematical expressions
+
+# # # # # #x = 45
+# # # # # #y = -3.5
+# # # # # #z = 2
+
+# # # # # #result_1 = round(x)
+# # # # # #result_2 = abs(y)
+# # # # # #result_3 = pow(2, 4)
+# # # # # #result_4 = max(x, y, z)
+# # # # # #result_5 = min(x, y, z)
+
+
+# # # # # #print(result_1)
+# # # # # #print(result_2)
+# # # # # #print(result_3)
+# # # # # #print(result_4)
+# # # # # #print(result_5)
+
+
+
+# # # # # #import math
+
+# # # # # #X = 24.6
+# # # # # #Y = 6.1
+
+# # # # # #print(math.pi)
+# # # # # #print(math.e)
+
+# # # # # #result1 = math.sqrt(X)
+# # # # # #result2 = math.ceil(Y)
+# # # # # #result3 = math.floor(Y)
+
+# # # # # #print(result3)
+
+
+# # # # # #import math
+
+# # # # # #radius = float(input("what is the radius of the circle?: "))
+# # # # # #pi_value = math.pi
+
+# # # # # #result1 = 2 * pi_value * radius
+# # # # # #result2 = math.pi * pow(radius, 2)
+
+# # # # # #print("congratulations you got your answer")
+# # # # # #print(f"your circumpherence of the circle is: {round(result1, 4)}")
+# # # # # #print(f"your area of the circle is: {round(result2, 3)}cm^2")
+
+
+# # # # # #a = float(input("What is the length of the perpendicular side? : "))
+# # # # # #b = float(input("What is the length of the base?: "))
+
+# # # # # #hypotenuse = math.sqrt(pow(a, 2) + pow(b, 2))
+
+# # # # # #print("congratulations you got your answer")
+# # # # # #print(f"the length of your hypotenuse is: {round(hypotenuse, 2)} cm^2")
+
+
+
+# # # # # # if statements
+
+
+# # # # # print("This is a checking of if your choices are eligible or not")
+# # # # # age = int(input("what is your age? : "))
+
+# # # # # final_age = age - 6
+
+# # # # # if final_age >= 18:
+
+# # # # #     print("You're good to go")
+
+# # # # # elif final_age >= 20:
+# # # # #     print("Nigga why you like 20 year?")
+# # # # #     print("like 12 years")
+
+# # # # # elif final_age >= 24:
+# # # # #     print("huhh what can i say...")
+
+# # # # # else:
+# # # # #     print(f"ohh you liked {final_age}")
+# # # # #     print("Haha i knew you liked Kids")
+# # # # #     print("You're not a Pedo")
+
+
+# # # # # name = input("what is yout name: ")
+
+# # # # # if name == "":
+# # # # #     print("You didn't put any name")
+
+# # # # # else:
+# # # # #     print(f"Hello {name} or I can say Nigga :)")
+
+
+# # # # operators = input("Enter an operator (+ - * /): ")
+# # # # num1 = float(input("Enter the first number: "))
+# # # # num2 = float(input("Enter the second number: "))
+
+# # # # if operators == "+":
+# # # #     result = num1 + num2
+# # # #     print(round(result, 2))
+# # # # elif operators == "-":
+# # # #     result = num1 - num2
+# # # #     print(round(result, 2))
+# # # # elif operators == "*":
+# # # #     result = num1 * num2
+# # # #     print(round(result, 2))
+# # # # elif operators == "/":
+# # # #     result = num1 / num2
+# # # #     print(round(result, 2))
+# # # # else:
+# # # #     print("Invalid operator")
+
+
+# # # weight = float(input("Enter your weight: "))
+# # # Unit = input("Enter the unit of your weight: (kg/lbs)")
+
+# # # if Unit == "kg":
+# # #     Result = weight * 2.205
+# # #     Unit = "lbs"
+# # #     print(f"Your weight in lbs is: {round(Result, 2)} {Unit}")
+
+# # # elif Unit == "lbs":
+# # #     Result = weight / 2.205
+# # #     Unit = "kg"
+# # #     print(f"your weight in kg is: {round(Result, 2)} {Unit}")
+
+# # # else:
+# # #     print("Invalid unit")
+    
+# # temp = float(input("Enter the temperature: "))
+# # raining = True
+
+# # if temp > 45 or raining:
+# #     print("It's too hot outside or it's raining, better stay indoors!")
+# # else:
+# #     print("The weather is nice, you can go outside and enjoy!")
+
+
+
+# # temp = float(input("Enter the temperature: "))
+# # is_raining = input("Is it raining? (yes/no): ").lower() == "yes"
+
+# # if temp > 43 and is_raining:
+# #     print("It's too hot and raining, better stay indoors!")
+
+# # elif temp < 5 and is_raining:
+# #     print("It's too cold and raining, better stay indoors!")
+
+# # elif 5 < temp < 43 and is_raining:
+# #     print("Its perfect weather but it's raining, you may go outside but take an umbrella with you!")
+
+# # elif 10 < temp < 15 and not is_raining:
+# #     print("It's too cold and raining, better stay indoors!")
+
+# # elif 1 < temp < 3 and not is_raining:
+# #     print("Its perfect weather and not raining, you can go outside and enjoy!")
+
+
+# # num = int(input("Enter a number: "))
+# # a = int(input("Enter the first number: "))
+# # b = int(input("Enter the second number: "))
+# # age = int(input("Enter your age: "))
+
+# # print( "Even" if num % 2 == 0 else "ODD")
+# # print(f"maximum number is: {a if a>b else b}")
+# # print(f"minimum number is: {a if a<b else b}")
+# # print("you're an adult" if age >= 18 else "you're a minor")
+
+
+# username = input("Enter your username: ")
+
+# username.isalpha()
+
+# if len(username) > 12:
+#     print("Username cant be long more than 12 words")
+# elif not username.find(" ") == -1:
+#     print("Username cant contain spaces")
+# elif not username.isalpha():
+#     print("Username can only contain letters")
+# else:
+#     print("Username is valid")
+
+# credit_card_number = input("Enter your credit card number: ")
+
+# print(credit_card_number[1:4])
+# print(credit_card_number[:-4])
+# print(credit_card_number[-4:])
+# print(credit_card_number[::3])
+# print(credit_card_number[3:7])
+
+
+# price_of_product = float(input("Enter the price of the product: "))
+
+# print(f"The price of this product is: ${price_of_product:.1f}")
+# print(f"The price of this product is: ${price_of_product:03}")
+
+# number = int(input("Enter a number between 5 and 10: "))
+
+# while (number <= 5 or number >= 10):
+#     print("Invalid number, please try again.")
+#     number = int(input("Enter a number between 5 and 10: "))
+
+# print(f"Congratulations! You entered a valid number: {number}")
+
+
+# from time import time
+
+
+# principle = 0
+# interest_rate = 0
+# time_period = 0
+# number_of_compoundings = 0
+
+# while principle <= 0:
+#     principle = float(input("Enter the initial principle amount: "))
+#     if principle <= 0:
+#         print("Principle amount must be greater than 0. Please try again.")
+
+# while interest_rate <= 0:
+#     interest_rate = float(input("Enter the annual interest rate (as a decimal): "))
+#     if interest_rate <= 0:
+#         print("Interest rate must be greater than 0. Please try again.")
+
+# while time_period <= 0:
+#     time_period = float(input("Enter the time period in years: "))
+#     if time_period <= 0:
+#         print("Time period must be greater than 0. Please try again.")
+
+# while number_of_compoundings <= 0:
+#     number_of_compoundings = int(input("Enter the number of times interest is compounded per year: "))
+#     if number_of_compoundings <= 0:
+#         print("Number of compoundings must be greater than 0. Please try again.")
+
+# Total_amount = principle * pow((1 + interest_rate/ number_of_compoundings ), time_period)
+
+# print(f"The final amount after {time_period} years is: ${Total_amount:.2f}")
+
+
+
+
+# principle = 0
+# interest_rate = 0
+# time_period = 0
+# number_of_compoundings = 0
+
+# while True:
+#     principle = float(input("Enter the initial principle amount: "))
+#     if principle <= 0:
+#         print("Principle amount must be greater than 0. Please try again.")
+#     else:
+#         break
+
+# while True:
+#     interest_rate = float(input("Enter the annual interest rate (as a decimal): "))
+#     if interest_rate <= 0:
+#         print("Interest rate must be greater than 0. Please try again.")
+#     else:
+#         break
+            
+# while True:
+#     time_period = float(input("Enter the time period in years: "))
+#     if time_period <= 0:
+#         print("Time period must be greater than 0. Please try again.")
+#     else:
+#         break
+
+# while True:
+#     number_of_compoundings = int(input("Enter the number of times interest is compounded per year: "))
+#     if number_of_compoundings <= 0:
+#         print("Number of compoundings must be greater than 0. Please try again.")
+#     else:
+#         break
+
+# Total_amount = principle * pow((1 + interest_rate/ number_of_compoundings ), time_period)
+
+# print(f"The final amount after {time_period} years is: ${Total_amount:.2f}")
+
+
+
+# for x in range(1,24):
+#     print(x)
+
+# for counter in reversed(range(1,25,2)):
+#     if counter % 2 == 0:
+#         break
+#     print(counter)
+
+# for counter in reversed(range(1,25,3)):
+#     if counter % 2 == 0:
+#         break
+#     print(counter)
+
+
+# credit_card_number = input("Enter your credit card number: ")
+
+# for credit_card_number in range(1, len(credit_card_number) +1):
+#     print(credit_card_number)
+
+
+import time
+
+
+
+
