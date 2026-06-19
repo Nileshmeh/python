9f0c970ca4048ea80d81e1ada93804fda60b351e
Nileshmeh <teengardener07@gmail.com>
2026-06-19 11:36:17 +0530

    updated the timecounter.py

--- timecounter.py
+++ timecounter.py
@@ -1,13 +1,25 @@
 import time
 
-my_time = input("Enter a time in seconds:")
+my_time = int(input("Enter the amount of time:"))
+unit = input("Enter the Unit in (seconds/minutes/hours/days)").lower()
+
+if unit in ["second", "seconds", "s"]:
+    total_seconds = my_time
+elif unit in ["minute", "minutes", "m"]:
+    total_seconds = my_time * 60
+elif unit in ["hour", "hours", "h"]:
+    total_seconds = my_time * 3600
+elif unit in ["day", "days", "d"]:
+    total_seconds = my_time * 86400
+    
 print("Nigga you dont have much time left!")
 
-for x in range (int(my_time), 0, -1):
+for x in range ((total_seconds), 0, -1):
     seconds = x % 60
     minutes = int(x / 60) % 60
-    hours = int(x / 3600)
-    print(f"you're remaining time is {hours:02}:{minutes:02}:{seconds:02}")
+    hours = int(x / 3600) % 24
+    days = int(x / 86400)
+    print(f"you're remaining time is {days:02}:{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
 
 print("Fuck you NIgga! Times up!!")
