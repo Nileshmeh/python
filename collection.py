cfa3056817d399a7d2884cb0755bae782092a302
Nileshmeh <teengardener07@gmail.com>
2026-06-21 21:19:53 +0530

    shopping cart python code

---
+++ collection.py
@@ -0,0 +1,5 @@
+Gay_niggas = ["Surindar", "Jogindar", "Rajinder"]
+
+print(Gay_niggas[0:2])
+print(Gay_niggas.count("Surindar"))
+
---
+++ newcode.py
@@ -0,0 +1,82 @@
+inventory = { 
+    "potato" : 30,
+    "onion" : 10,
+    "cauliflower" : 60,
+    "tomato" : 50,
+    "spinach" : 5,
+    "mango" : 80,
+    "strawberry" : 150,
+    "pineapple" : 60,
+    "papaya" : 35,
+    "cucumber" : 15,
+    "lettuce" : 10,
+    "apple" : 250,
+}
+
+cart = {}
+
+print("Welcome to the shop")
+print("Items that are available are:")
+print(", ".join(inventory.keys()))
+print("press Q to quit any part of shopping")
+
+print("-" * 40)
+
+while True:
+    user_choice = input("add the items you want to add to the cart: ").strip().lower()
+    
+    if user_choice == "q":
+        print("proceeding to checkout")
+        break
+    
+    if user_choice in inventory:
+        price_per_unit = inventory[user_choice]
+        print(f"your {user_choice} will cost you {price_per_unit} per item")
+
+        quantity_input = input(f"In how much amount do you want {user_choice}:  ").strip()
+
+        if quantity_input.lower() == "q":
+            print("proceeding to checkout")
+            break
+
+        if quantity_input.isdigit() and int(quantity_input) > 0:
+            quantity = int(quantity_input)
+            item_total_price = quantity * price_per_unit
+
+            if user_choice in cart:
+                # 3. Cleaned up variables to make sure they all use integers
+                cart[user_choice]['quantity'] += quantity
+                cart[user_choice]['total_price'] += item_total_price
+
+            else:
+                cart[user_choice] = {
+                   'quantity' : quantity,
+                   'total_price' : item_total_price
+                }
+
+            print(f"\n[ADDED] {quantity}x {user_choice.capitalize()} to your cart.")
+            print(f"Current unique items in cart: {', '.join(cart.keys())}\n")
+        
+        else:
+            print("invalid quantity\n")
+
+    else:
+        print(f"sorry the {user_choice} is not available in the store. choose something else!\n")
+
+
+print("-" * 40)
+print("       Your final receipt      ")
+print("-" * 40)
+
+if not cart:
+    print("your cart is empty your total bill is 0")
+
+else:
+    grand_total = 0
+    print(f"{'Item':<15} | {'Quantity':<10} | {'Subtotal':<10}")
+    print("-" * 40)
+    for item, details in cart.items():
+        print(f"{item.capitalize():<15} | {details['quantity']:<10} | {details['total_price']:<10}")
+        grand_total += details['total_price']
+    print("-" * 40)
+    print(f"Grand Total: {grand_total}")
