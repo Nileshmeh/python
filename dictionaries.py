458ed8881f48ffdbc02ce77deaa3553229d10b90
Nileshmeh <teengardener07@gmail.com>
2026-06-24 21:16:06 +0530

    dictionaries in python

---
+++ dictionaries.py
@@ -0,0 +1,28 @@
+india_capitals = {"West Bengal" : "Kolkata",
+                  "Delhi" : "New Delhi",
+                  "Jharkhand" : "Ranchi",
+                  "Andaman" : "Portblair",
+                  }
+
+
+# capitals = india_capitals.get("West Bengal")
+# india_capitals.update({"Delhi" : "Old delhi"})
+# india_capitals.pop("Delhi")
+# india_capitals.update({"Assam" : "Guwahati"})
+# india_capitals.popitem()
+
+
+# value = india_capitals.values()
+
+# for value in india_capitals.values():
+#     print(value)
+
+# items = india_capitals.items()
+
+for key, values in india_capitals.items():
+    print(f"{key} : {values}")
+                
+                  
+                  
+                  
+                
